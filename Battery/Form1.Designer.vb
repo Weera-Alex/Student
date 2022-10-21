3eaeb54d50b61c34319488d5ce83cd0d77a0dff3
catsyndicate <xanderencio@gmail.com>
2022-10-21 22:55:37 +0800

    Add project files.

---
+++ Battery/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.id = New System.Windows.Forms.TextBox()
+        Me.studentname = New System.Windows.Forms.TextBox()
+        Me.Title = New System.Windows.Forms.TextBox()
+        Me.Genre = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TitleDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GenreDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BatteryTableBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MovieExamDataSet = New Battery.movieExamDataSet()
+        Me.BatteryTableTableAdapter = New Battery.movieExamDataSetTableAdapters.BatteryTableTableAdapter()
+        Me.Button5 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BatteryTableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MovieExamDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'id
+        '
+        Me.id.Location = New System.Drawing.Point(217, 159)
+        Me.id.Name = "id"
+        Me.id.ReadOnly = True
+        Me.id.Size = New System.Drawing.Size(161, 22)
+        Me.id.TabIndex = 0
+        '
+        'studentname
+        '
+        Me.studentname.Location = New System.Drawing.Point(217, 212)
+        Me.studentname.Name = "studentname"
+        Me.studentname.Size = New System.Drawing.Size(161, 22)
+        Me.studentname.TabIndex = 1
+        '
+        'Title
+        '
+        Me.Title.Location = New System.Drawing.Point(217, 263)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(161, 22)
+        Me.Title.TabIndex = 2
+        '
+        'Genre
+        '
+        Me.Genre.Location = New System.Drawing.Point(217, 312)
+        Me.Genre.Name = "Genre"
+        Me.Genre.Size = New System.Drawing.Size(161, 22)
+        Me.Genre.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(26, 435)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(125, 52)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "ADD"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(185, 435)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(125, 52)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "EDIT"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(350, 435)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(125, 52)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "DELETE"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(1003, 70)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(125, 52)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "SEARCH"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(576, 85)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(392, 22)
+        Me.TextBox5.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(66, 159)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 24)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(66, 212)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 24)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Studentname"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(66, 263)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 24)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Title"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(66, 312)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 24)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Genre"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.TitleDataGridViewTextBoxColumn, Me.GenreDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.BatteryTableBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(549, 146)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(731, 454)
+        Me.DataGridView1.TabIndex = 13
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "Id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "Id"
+        Me.IdDataGridViewTextBoxColumn.MinimumWidth = 6
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        Me.IdDataGridViewTextBoxColumn.Width = 125
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.MinimumWidth = 6
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        Me.NameDataGridViewTextBoxColumn.Width = 125
+        '
+        'TitleDataGridViewTextBoxColumn
+        '
+        Me.TitleDataGridViewTextBoxColumn.DataPropertyName = "Title"
+        Me.TitleDataGridViewTextBoxColumn.HeaderText = "Title"
+        Me.TitleDataGridViewTextBoxColumn.MinimumWidth = 6
+        Me.TitleDataGridViewTextBoxColumn.Name = "TitleDataGridViewTextBoxColumn"
+        Me.TitleDataGridViewTextBoxColumn.Width = 125
+        '
+        'GenreDataGridViewTextBoxColumn
+        '
+        Me.GenreDataGridViewTextBoxColumn.DataPropertyName = "Genre"
+        Me.GenreDataGridViewTextBoxColumn.HeaderText = "Genre"
+        Me.GenreDataGridViewTextBoxColumn.MinimumWidth = 6
+        Me.GenreDataGridViewTextBoxColumn.Name = "GenreDataGridViewTextBoxColumn"
+        Me.GenreDataGridViewTextBoxColumn.Width = 125
+        '
+        'BatteryTableBindingSource
+        '
+        Me.BatteryTableBindingSource.DataMember = "BatteryTable"
+        Me.BatteryTableBindingSource.DataSource = Me.MovieExamDataSet
+        '
+        'MovieExamDataSet
+        '
+        Me.MovieExamDataSet.DataSetName = "movieExamDataSet"
+        Me.MovieExamDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'BatteryTableTableAdapter
+        '
+        Me.BatteryTableTableAdapter.ClearBeforeFill = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(1155, 70)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(125, 52)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "REFRESH"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1308, 683)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Genre)
+        Me.Controls.Add(Me.Title)
+        Me.Controls.Add(Me.studentname)
+        Me.Controls.Add(Me.id)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BatteryTableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MovieExamDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents id As TextBox
+    Friend WithEvents studentname As TextBox
+    Friend WithEvents Title As TextBox
+    Friend WithEvents Genre As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents MovieExamDataSet As movieExamDataSet
+    Friend WithEvents BatteryTableBindingSource As BindingSource
+    Friend WithEvents BatteryTableTableAdapter As movieExamDataSetTableAdapters.BatteryTableTableAdapter
+    Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TitleDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents GenreDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Button5 As Button
+End Class
---
+++ Battery/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private cmd As New SqlCommand
+    Private ReadOnly cn As New SqlConnection("Data Source=.;Initial Catalog=scream;Integrated Security=True;Pooling=False")
+    Private adapt As SqlDataAdapter
+    Private dt As DataTable
+
+    Public Sub New()
+        InitializeComponent()
+        DisplayData()
+    End Sub
+
+    Public Sub DisplayData() 'display all request list of current user
+        cn.Open()
+        Dim dt As New DataTable()
+        adapt = New SqlDataAdapter("select * from BatteryTable", cn)
+        adapt.Fill(dt)
+        DataGridView1.DataSource = dt
+        cn.Close()
+    End Sub
+
+    Public Sub cleartextboxes()
+        id.Text = ""
+        studentname.Text = ""
+        Title.Text = ""
+        Genre.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If studentname.Text <> "" AndAlso Title.Text <> "" AndAlso Genre.Text <> "" Then
+            Dim code As String
+            Dim random As New Random()
+            Dim value As Integer
+            value = random.Next(0, 99999)
+            studentname.Text.ToUpper()
+            If studentname.Text.Count > 0 Then
+                Dim remove As String = id.Text.ToUpper.Replace(" ", "")
+                remove = remove.Replace("A", "").Replace("E", "").Replace("I", "").Replace("O", "").Replace("U", "").Replace("1", "").
+                    Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").
+                    Replace("8", "").Replace("9", "").Replace("0", "")
+                code = Format(Date.Now, "yyyy") & remove & value
+                id.Text = code
+            End If
+            cmd = New SqlCommand("insert into BatteryTable values(@id,@studentname,@title,@genre)", cn)
+            cn.Open()
+            cmd.Parameters.Add("@id", SqlDbType.NVarChar).Value = id.Text
+            cmd.Parameters.Add("@studentname", SqlDbType.NVarChar).Value = studentname.Text
+            cmd.Parameters.Add("@title", SqlDbType.NVarChar).Value = Title.Text
+            cmd.Parameters.Add("@genre", SqlDbType.NVarChar).Value = Genre.Text
+            cmd.ExecuteNonQuery()
+            cn.Close()
+            DisplayData()
+            cleartextboxes()
+            MessageBox.Show("added succesfully")
+        Else
+            MessageBox.Show("fill all fields", "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If id.Text <> "" AndAlso studentname.Text <> "" AndAlso Title.Text <> "" AndAlso Genre.Text <> "" Then
+            cmd = New SqlCommand("update BatteryTable set Id=@studentId,Name=@studentName,
+            Title=@title,Genre=@genre where Id=@studentId", cn)
+            cn.Open()
+            cmd.Parameters.Add("@studentId", SqlDbType.NVarChar).Value = id.Text
+            cmd.Parameters.Add("@studentName", SqlDbType.NVarChar).Value = studentname.Text
+            cmd.Parameters.Add("@title", SqlDbType.NVarChar).Value = Title.Text
+            cmd.Parameters.Add("@genre", SqlDbType.NVarChar).Value = Genre.Text
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("edited succesfully")
+            cn.Close()
+            DisplayData()
+            cleartextboxes()
+        Else
+            MessageBox.Show("fill all fields")
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        id.Text = DataGridView1.Rows(e.RowIndex).Cells(0).Value.ToString()
+        studentname.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value.ToString()
+        Title.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value.ToString()
+        Genre.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value.ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If id.Text <> "" Then
+
+            cmd = New SqlCommand("delete BatteryTable where id=@Id", cn)
+            cn.Open()
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id.Text
+            cmd.ExecuteNonQuery()
+            cn.Close()
+            DisplayData()
+            cleartextboxes()
+            MessageBox.Show("deleted succesfully")
+        Else
+            MessageBox.Show("fill all fields")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If TextBox5.Text IsNot String.Empty Then
+            cn.Open()
+            adapt = New SqlDataAdapter("Select * from BatteryTable where title like '%' + @search +  '%'
+            Or genre Like '%' + @search +  '%' Or Id Like '' + @search +  '%'", cn)
+            adapt.SelectCommand.Parameters.Add("@search", SqlDbType.NVarChar).Value = TextBox5.Text
+            dt = New DataTable()
+            adapt.Fill(dt)
+            DataGridView1.DataSource = dt
+            cn.Close()
+        Else
+            MessageBox.Show("fill all fields")
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        DisplayData()
+    End Sub
+
+End Class
